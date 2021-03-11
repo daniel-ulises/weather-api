600cf1deaffd2dc146c86f4cb2ca53d943c13b57
Daniel Ulises <daniel.cau@pm.me>
2021-03-11 23:54:26 +0100

    Cleanup of some unused code

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -4,7 +4,7 @@ function Display(props) {
 	const cards = []
 	const generateCards = () => {
 		for(let i = 0; i < props.city.list.length; i += 8) {
-				cards.push(
+			cards.push(
 				<div className="info">
 					<div className="temp-container">
 						<div className="title">
@@ -21,10 +21,10 @@ function Display(props) {
 							</div>	
 						</div>
 						<div className="extra-info">
-								<img src={`https://openweathermap.org/img/wn/${props.city.list[i].weather[0].icon}@2x.png`}></img>
-								<span>{props.city.list[i].weather[0].description}</span>
-								<span>Feels like: {props.city.list[i].main.feels_like}°</span>
-								<span>Humidity: {props.city.list[i].main.humidity}%</span>
+							<img src={`https://openweathermap.org/img/wn/${props.city.list[i].weather[0].icon}@2x.png`}></img>
+							<span>{props.city.list[i].weather[0].description}</span>
+							<span>Feels like: {props.city.list[i].main.feels_like}°</span>
+							<span>Humidity: {props.city.list[i].main.humidity}%</span>
 						</div>					
 					</div>
 				</div>
@@ -37,30 +37,8 @@ function Display(props) {
 
 return (
 	<div className="container">
-				{generateCards()} 
-				{/* <div className="temp-container">
-					<div className="title">
-						<h1>{props.city.city.name}</h1>
-						<h4>{(props.currentDay(new Date(props.city.list[0].dt_txt).getDay()))}</h4>
-					</div>
-					<div className="temp-display">
-						<div className="main-temp">
-							<span className="temp">{props.city.list[0].main.temp}°</span>
-							<div className="min-max">
-								<span>{props.city.list[0].main.temp_min}°</span>
-								<span>{props.city.list[0].main.temp_max}°</span>
-							</div>
-						</div>	
-					</div>
-					<div className="extra-info">
-							<img src={`https://openweathermap.org/img/wn/${props.city.list[0].weather[].icon}@2x.png`}></img>
-							<span>{props.city.list[0].weather[0].description}</span>
-							<span>Feels like: {props.city.list[0].main.feels_like}°</span>
-							<span>Humidity: {props.city.list[0].main.humidity}%</span>
-					</div>					
-				</div> */}
-
-		</div>
+		{generateCards()} 
+	</div>
 	)
 }
 
